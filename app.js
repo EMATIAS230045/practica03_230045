ccfc03c782dd89c4f746d8d8db6f18339284bfea
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2025-01-14 10:23:05 -0600

    Practica 3 Sessiones Activas
    En esta practica repasamos contenido de como manejar el inicio de sessiones, su estado y destruccion

--- app.js
+++ app.js
@@ -1,76 +1,82 @@
-import express from "express";
-import session from "express-session";
+import express from 'express';
+import session from 'express-session';
+import moment from 'moment-timezone'
 
-const app = express(); 
+const app = express();
 
-app.use (
+// Configuración del middleware de sesiones
+
+app.use(
     session({
-        secret:'P03-GM_Bots',
+        secret:'P3-MARCH#BOTsito-sesionesPersistenetes',
         resave: false,
-        saveUnintilized:true,
-        cookie:{maxage: 24 * 60 * 100},
-     })
-);
-// ? Ruta para iniciar la session 
-app.get("/iniciar-sesion", (req, res)=> {
-    if(!req.session.inicio){
-        req.session.inicio = new Date();
-        req.session.inicio = new Date();
-        res.send('session Iniciada. ');
-    }else{
-        res.send('La session esta acticvada.')
+        saveUninitialized: true,
+        cookie: { maxAge: 24 * 60 * 60 * 1000} // 1 día
+    })
+)
+
+// Ruta para inicializar la sesión
+app.get('/iniciar-sesion',(req,res)=>{
+    if (!req.session.inicio){
+        req.session.inicio=new Date();
+        req.session.ultimoAcceso=new Date();
+        res.send('Sesión Iniciada.');
+    } else {
+        res.send('La sesión ya está activa.');
     }
 })
 
-app.get('/Actualizacion' , (req, res) => {
-    if(req.session.inicio){
-        req.session.ultimoAcceso = new  Date();
-        res.send('fecha de ultima concsulta actualizada')
-    }else{
-        res.send('no hay ninguna session activa.')
+// Ruta para actualizar la fecha de última consulta
+app.get('/actualizar',(req,res)=>{
+    if (req.session.inicio){
+        req.session.ultimoAcceso = new Date();
+        res.send('Fecha de última consulta actualizada.');
+    } else {
+        res.send('No hay una sesión activa.');
     }
 })
 
-app.get('/Estado-session' , (req, res) => {
-    if(req.session.inicio){
-        const inicio = req.session.inicio;
-        const ultimoAcceso = req.session.ultimoAcceso;
-        const ahora  = new  Date ();
-        
-        //calcular la antiguedad de la session
-        const antiguedadMs = ahora - inicio
-        const horas = Math.floor(antiguedadMs/(1000 * 60  * 60));
-        const minutos = Math.floor(antiguedadMs % (1000 * 60  * 60)/ (1000 *60));
-        const segundos = Math.floor(antiguedadMs % (1000 * 60)/ 1000)
+// Ruta para ver el estado de la sesión
+app.get('/estado-sesion', (req,res)=>{
+    if (req.session.inicio){
+        const inicio = new Date(req.session.inicio);
+        const ultimoAcceso = new Date(req.session.ultimoAcceso);
+        const ahora = new Date();
 
+        // Calcular la antiguedad de la sesión
+        const antiguedadMs = ahora - inicio;
+        const horas = Math.floor(antiguedadMs/(1000 * 60 * 60));
+        const minutos = Math.floor((antiguedadMs%(1000*60*60))/(1000*60));
+        const segundos = Math.floor((antiguedadMs%(1000*60))/1000);
+
+        // Convertimos las fechas al huso horario de CDMX
+        const inicioCDMX = moment(inicio).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss')
+        const ultimoAccesoCDMX = moment(ultimoAcceso).tz('America/Mexico_City').format('YYYY-MM-DD HH:mm:ss')
         res.json({
-            mensaje: 'Estado de la session',
-            sessionID: req.sessionID,
-            inicio: inicio.toISOString(),
-            ultimoAcceso: ultimoAcceso.toISOString(),
+            mensaje: 'Estado de la sesión',
+            sesionTD: req.sessionID,
+            inicio: inicioCDMX,
+            ultimoAcceso: ultimoAccesoCDMX,
             antiguedad: `${horas} horas, ${minutos} minutos, ${segundos} segundos`
         })
-    }else{
-        res.send('no hay ninguna session activa.')
+    } else {
+        res.send('No hay una sesión activa')
     }
 })
-//Ruta para cerrar session
-
-app.get('/cerrar-sesion', (req, res) => {
-    if(req.session){
-        req.session.destroy((err) => {
+// Ruta para cerrar la sesión
+app.get('/cerrar-sesion', (req,res)=>{
+    if (req.session.inicio){
+        req.session.destroy((err)=>{
             if(err){
-                return res.status(500).send('Error al cerrar la session ')
+                return res.status(500).send('Error al iniciar la sesión')
             }
-            return res.send('session cerrada correctamente')
-        })
-    }else{
-        res.send('no hay ninguna session activa para cerrar.')
+            res.send('Sesión cerrada correctamente')
+        });
+    } else {
+        res.send('No hay una sesión activa para cerrar.')
     }
-} )
-
-// iniciar el servidor
-const PORT = 3000
-app.listen(PORT, () =>{
-    console.log(`Servidor Ejecutandfose en servidor en htttp://localhost:${PORT}`)
+})
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`El servidor se ejecuto correctamente en el puerto ${PORT}`)
 })
